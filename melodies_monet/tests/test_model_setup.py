cec3c701a1b032e3af2739c493c9195be2a2ab18
dwf_ncar <fillmore@ucar.edu>
2022-07-10 11:22:50 -0600

    Added time dimension to random test fields.

--- melodies_monet/tests/test_model_setup.py
+++ melodies_monet/tests/test_model_setup.py
@@ -33,20 +33,21 @@ Read YAML control
 with open(args.control, 'r') as f:
     control = yaml.safe_load(f)
 
+"""
+Generate datetime arrays
+"""
 start_time = pd.to_datetime(control['analysis']['start_time'])
 end_time = pd.to_datetime(control['analysis']['end_time'])
 datetime_indices = pd.date_range(start_time, end_time,
     freq=control['test_setup']['freq'])
 ntime = len(datetime_indices)
 datetimes = [np.datetime64(dt) for dt in datetime_indices]
-print(datetimes)
 
 """
 Generate uniform grid
 """
 nlat = control['test_setup']['grid']['nlat']
 nlon = control['test_setup']['grid']['nlon']
-logging.info((nlat, nlon))
 lat_edges = np.linspace(-90, 90, nlat+1, endpoint=True, dtype=float)
 lat = 0.5 * (lat_edges[0:nlat] + lat_edges[1:nlat+1])
 lat_min, lat_max = lat_edges[0:nlat], lat_edges[1:nlat+1]
@@ -55,6 +56,12 @@ weight = np.abs(np.sin(deg_to_rad * lat_max) - np.sin(deg_to_rad * lat_min))
 lon_edges = np.linspace(-180, 180, nlon+1, endpoint=True, dtype=float)
 lon = 0.5 * (lon_edges[0:nlon] + lon_edges[1:nlon+1])
 
+logging.info((ntime, nlat, nlon))
+
+"""
+Generate xarray data arrays
+"""
+time_da = xr.DataArray(datetimes, attrs={'longname': 'datetime'})
 lat_da = xr.DataArray(lat, attrs={'longname': 'latitude', 'units': 'deg North'})
 lon_da = xr.DataArray(lon, attrs={'longname': 'longitude', 'units': 'deg East'})
 
@@ -66,10 +73,10 @@ field_names = control['model']['test_model']['variables'].keys()
 ds_dict = dict()
 for field_name in field_names:
     units = control['model']['test_model']['variables'][field_name]['units']
-    field = np.random.rand(nlat, nlon)
+    field = np.random.rand(ntime, nlat, nlon)
     field_da = xr.DataArray(
-        field, coords=[lat_da, lon_da],
-        dims=['lat', 'lon'], attrs={'units': units})
+        field, coords=[time_da, lat_da, lon_da],
+        dims=['time', 'lat', 'lon'], attrs={'units': units})
     ds_dict[field_name] = field_da
 ds = xr.Dataset(ds_dict)
 ds.to_netcdf(control['model']['test_model']['files'])
