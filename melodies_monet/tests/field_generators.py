8bc2789a9d92fb211ef0f9e064f1b38885e55358
dwf_ncar <fillmore@ucar.edu>
2022-07-14 14:43:54 -0600

    Start on field_generators.gaussian_plume_2d.

--- melodies_monet/tests/field_generators.py
+++ melodies_monet/tests/field_generators.py
@@ -10,12 +10,19 @@ def gaussian_plume_2d(lon_da, lat_da, time_da, params):
         lat_da: latitude array (xarray.DataArray)
         time_da: datetime array (xarray.DataArray)
         params: Gaussian distribution parameters (dictionary)
-            lon0: initial center longitude (float)
-            lat0: initial center latitude (float)
-            u0: constant eastward center velocity (float)
-            v0: constant northward center velocity (float)
+            lon_0: initial center longitude (float, degrees East)
+            lat_0: initial center latitude (float, degrees North)
+            u_0: constant eastward center velocity (float)
+            v_0: constant northward center velocity (float)
             sigma: standard deviation (float)
     returns:
-       field (xarray.DataArray) 
+        field (xarray.DataArray)
     """
-    pass
+
+    # great circle X(t) = cos(omega t) X_0 + sin(omega t) V_0
+
+    theta_0 = np.pi * (1 - params['lat_0'] / 180)
+    phi_0 = np.pi * params['lon_0'] / 180
+    X_0 = np.array([np.sin(theta_0) * np.cos(phi_0),
+                    np.sin(theta_0) * np.sin(phi_0),
+                    np.cos(theta_0)])
